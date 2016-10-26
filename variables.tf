d008ef539501bb83c13d5748cb6470c2b37dc50e
Konrad Rozycki <konrad.rozycki@voipnews.pl>
2016-10-26 10:49:38 +0100

    My initial commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+        default = "certs/us-west-2"
+}
+
+variable "public_key_path" {
+        default = "certs/tf.pub"
+}
+
+
+variable "key_name" {
+        default = "eskey2"
+}
+
+variable "key_path" {
+    default = "certs/tf.pem"
+
