c272fd8e594479651a64c938e874b1ff4407739a
Konrad Rozycki <konrad.rozycki@voipnews.pl>
2016-10-26 11:48:46 +0100

    VPC configuration

--- main.tf
+++ main.tf
@@ -119,6 +119,26 @@ resource "aws_instance" "es_node_02" {
   }
 }
 
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
 
 # Acces to  port 9200 from an internal network
 resource "aws_security_group" "default" {
